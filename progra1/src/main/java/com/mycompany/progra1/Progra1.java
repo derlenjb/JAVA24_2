d043364d6c525276fb1c0a293dca5da4aa7206cf
derlenjb <lenin.jaramillo.b@uni.pe>
2024-10-11 17:33:57 -0500

    Añadir archivos iniciales

---
+++ progra1/src/main/java/com/mycompany/progra1/Progra1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progra1;
+
+/**
+ *
+ * @author User
+ */
+public class Progra1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
